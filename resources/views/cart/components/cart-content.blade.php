7f3a18c5bd61e5e5a29522752869feb32951c4a3
Paco Jaez <pacojaez@gmail.com>
2021-07-25 11:17:32 +0200

    preparing views structure

---
+++ resources/views/cart/components/cart-content.blade.php
@@ -0,0 +1,99 @@
+<div>
+    <div class="container flex flex-col p-2 mx-auto lg:h-full lg:py-4 lg:flex-row lg:items-start lg:space-x-6">
+        <div class="flex flex-col items-center justify-center w-full lg:w-1/2">
+            <!--cart item-->
+            <div class="flex flex-col items-center justify-center w-full">
+                <div class="flex flex-row items-center justify-center mt-6 md:flex-row">
+                    <div
+                        class="flex flex-col justify-center my-2 mr-0 transition-all duration-1000 ease-in-out md:mr-2">
+                        <img class="object-cover w-full h-full max-w-2xl rounded-md"
+                            src="https://images.unsplash.com/photo-1493863641943-9b68992a8d07?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=739&q=80"
+                            alt="glasses photo">
+                    </div>
+                    <div
+                        class="flex flex-col justify-center w-full mx-0 my-2 transition-all duration-1000 ease-in-out md:mx-4">
+                        <h3>CHAMPION OUTFIT</h3>
+                        <h4>Units: 1 unidad</h4>
+                        <h4>Price: 30 €</h4>
+                    </div>
+                </div>
+            </div>
+            <!--cart itam-->
+            <div class="flex flex-col items-center justify-center w-full">
+                <div class="flex flex-row items-center justify-center mt-6 md:flex-row">
+                    <div
+                        class="flex flex-col justify-center my-2 mr-0 transition-all duration-1000 ease-in-out md:mr-2">
+                        <img class="object-cover w-full h-full max-w-2xl rounded-md"
+                            src="https://images.unsplash.com/photo-1493863641943-9b68992a8d07?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=739&q=80"
+                            alt="glasses photo">
+                    </div>
+                    <div
+                        class="flex flex-col justify-center w-full mx-0 my-2 transition-all duration-1000 ease-in-out md:mx-4">
+                        <h3>CHAMPION OUTFIT</h3>
+                        <h4>Units: 1 unidad</h4>
+                        <h4>Price: 30 €</h4>
+                    </div>
+                </div>
+            </div>
+            <!--cart itam-->
+            <div class="flex flex-col items-center justify-center w-full">
+                <div class="flex flex-row items-center justify-center mt-6 md:flex-row">
+                    <div
+                        class="flex flex-col justify-center my-2 mr-0 transition-all duration-1000 ease-in-out md:mr-2">
+                        <img class="object-cover w-full h-full max-w-2xl rounded-md"
+                            src="https://images.unsplash.com/photo-1493863641943-9b68992a8d07?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=739&q=80"
+                            alt="glasses photo">
+                    </div>
+                    <div
+                        class="flex flex-col justify-center w-full mx-0 my-2 transition-all duration-1000 ease-in-out md:mx-4">
+                        <h3>CHAMPION OUTFIT</h3>
+                        <h4>Units: 1 unidad</h4>
+                        <h4>Price: 30 €</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-row items-center justify-center w-full mt-4 lg:w-1/2">
+                <input type="text" class="border rounded border-3 border-purple">
+                <button
+                    class="px-8 py-3 mb-1 mr-1 text-base font-bold text-white uppercase transition-all duration-150 ease-linear bg-purple-500 rounded shadow-md outline-none active:bg-purple-600 hover:shadow-lg focus:outline-none"
+                    type="button">
+                    <i class="fas fa-gem"></i> APPLY
+                </button>
+            </div>
+            <div class="flex flex-row items-center justify-center w-full mt-4">
+                <div class="flex flex-row items-center justify-center w-full">
+                    <h2 class="p-4 m-2 text-2xl font-bold">TOTAL: 35,45 €</h2>
+                    <h4 class="p-4 m-2 text-sm font-bold">* Shipping is calculated at checkout</h4>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col items-center justify-center w-full lg:w-1/2">
+            <div class="border border-purple-500 rounded border-3 ">
+                <h2 class="p-4 m-2 text-4xl font-extrabold">TOTAL WEIGHT: 15 KG</h2>
+            </div>
+            <div class="mt-4 ">
+                <img class="object-cover w-2/3 max-w-2xl rounded-md h-2/3"
+                    src="https://cdn.pixabay.com/photo/2017/02/20/16/50/balance-2083206_960_720.png"
+                    alt="glasses photo">
+            </div>
+        </div>
+    </div>
+    <div
+        class="flex flex-col justify-center w-full p-2 m-auto mx-4 font-sans text-center bg-grey-light md:flex-row">
+        <input type="text" class="m-2 border rounded border-3 border-purple" placeholder="">
+        <button
+            class="px-8 py-3 m-2 mb-1 mr-1 text-base font-bold text-white uppercase transition-all duration-150 ease-linear bg-purple-500 rounded shadow-md outline-none active:bg-purple-600 hover:shadow-lg focus:outline-none"
+            type="button">
+            <i class="fas fa-gem"></i> KEEP BUYING
+        </button>
+        <button
+            class="px-8 py-3 m-2 mb-1 mr-1 text-base font-bold text-white uppercase transition-all duration-150 ease-linear bg-purple-500 rounded shadow-md outline-none active:bg-purple-600 hover:shadow-lg focus:outline-none"
+            type="button">
+            <i class="fas fa-gem"></i> CHECK OUT
+        </button>
+        <label class="inline-flex items-center m-2">
+            <input type="checkbox" class="form-checkbox" checked>
+            <span class="ml-2">I agree, no returns, refunds or exchange </span>
+        </label>
+    </div>
+</div>
---
+++ resources/views/partials/breadcrumbs.blade.php
@@ -0,0 +1,9 @@
+<div class="w-full p-2 mx-4 font-sans rounded bg-grey-light">
+    <ol class="flex list-reset text-grey-dark">
+        <li><a href="#" class="font-bold">Home</a></li>
+        <li><span class="mx-2">></span></li>
+        <li><a href="#" class="font-bold">Library</a></li>
+        <li><span class="mx-2">></span></li>
+        <li>Data</li>
+    </ol>
+</div>

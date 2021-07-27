f69991076dd9f403382634c0372099bd75eb1cb0
Paco Jaez <pacojaez@gmail.com>
2021-07-27 22:05:36 +0200

    added color change to wishlist

--- app/Http/Livewire/NavWishlist.php
+++ app/Http/Livewire/NavWishlist.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Facades\Auth;
 class NavWishlist extends Component
 {
     public $count; //number of items in the wishlist
+    public $hasWishlist= false; //properties to change the color of the wishlist in the mount function
+    public $noHasWishlist= true; //properties to change the color of the wishlist in the mount function
 
     protected $listeners =[
         'refresh' => 'mount'
@@ -17,6 +19,10 @@ class NavWishlist extends Component
     public function mount(){
 
         $this->count = \App\Models\WishlistItem::where('user_id', 'like', Auth::user()->id)->count();
+        if($this->count > 0){
+            $this->hasWishlist = true;
+            $this->noHasWishlist = false;
+        }
     }
 
     public function render()

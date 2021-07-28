a849be11dbf45d49aef5fe2b24030e39e40f1740
Paco Jaez <pacojaez@gmail.com>
2021-07-28 13:32:59 +0200

    working on middleware isAdmin

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\IsAdmin::class,
+            // \App\Http\Middleware\IsAdmin::class,
         ],
 
         'api' => [
@@ -64,6 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'isAdmin' => \App\Http\Middleware\IsAdmin::class,
+        // 'isAdmin' => \App\Http\Middleware\IsAdmin::class,
     ];
 }
